10cfab2ce31a98338401d2ed85f5c62571e3e6be
Sergey <mr.ultimateground@gmail.com>
2023-10-14 21:47:27 +0600

    admin panel added

--- index/admin.py
+++ index/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from index.models import Person, Course, Grade
-@admin.register(Person)
+from index.models import Event, Category
+@admin.register(Category)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Event)
 class PersonAdmin(admin.ModelAdmin):
     pass
